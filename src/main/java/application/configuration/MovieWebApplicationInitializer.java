32040c1dc285215062759b305435f0b257362954
martinatanasov <martinatanasovmusala@gmail.com>
2018-01-21 01:28:57 +0200

    MovieWebApp configured to start on tomcat

---
+++ src/main/java/application/configuration/MovieWebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package application.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MovieWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { MovieWebApplicationConfiguration.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+}
