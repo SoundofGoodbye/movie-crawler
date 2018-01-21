5938c627974eb8c88cc08720fdebcb38d815f6ae
martinatanasov <martinatanasovmusala@gmail.com>
2018-01-21 17:19:28 +0200

    Created project.properties file and added configuration to load it by Spring. Property used in MovieService for example

--- src/main/java/application/configuration/MovieWebApplicationConfiguration.java
+++ src/main/java/application/configuration/MovieWebApplicationConfiguration.java
@@ -3,6 +3,7 @@ package application.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "application")
+@PropertySource("classpath:project.properties")
 public class MovieWebApplicationConfiguration implements WebMvcConfigurer {
     @Bean
     public ViewResolver viewResolver() {
