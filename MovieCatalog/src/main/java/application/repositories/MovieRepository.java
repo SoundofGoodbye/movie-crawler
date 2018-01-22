4898fe780db074a89d15d264114b7e8c32e8c75c
martinatanasov <martinatanasovmusala@gmail.com>
2018-01-22 02:47:08 +0200

    Refactored MovieRepo

--- MovieCatalog/src/main/java/application/repositories/MovieRepository.java
+++ MovieCatalog/src/main/java/application/repositories/MovieRepository.java
@@ -3,6 +3,6 @@ package application.repositories;
 import application.entities.Movie;
 import org.springframework.data.repository.CrudRepository;
  
-public interface CountryRepository extends CrudRepository<Movie,Integer> {
+public interface MovieRepository extends CrudRepository<Movie,Integer> {
     
 }
