b63313f23a328fb82a3ae7cec117825be6402be0
Kaloyan-Stankov <kalo944@gmail.com>
2018-01-25 03:10:14 +0200

    Removed unused SiteDTO and forgotten imports.

--- MovieCatalogCrawler/src/main/java/moviecrawler/crawlers/BasicWebCrawler.java
+++ MovieCatalogCrawler/src/main/java/moviecrawler/crawlers/BasicWebCrawler.java
@@ -1,18 +1,12 @@
 package moviecrawler.crawlers;
 
 import moviecrawler.dtos.MovieDTO;
-import moviecrawler.dtos.SiteDTO;
 import moviecrawler.strategy.SiteStrategy;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BasicWebCrawler {
     private HashSet<String> links;
