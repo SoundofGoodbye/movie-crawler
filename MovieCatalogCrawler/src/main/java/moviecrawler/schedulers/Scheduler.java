35dffe0699855c53bbe10f86940e13e9f694b3f5
Valentin Dimitrov <valjo94@abv.bg>
2018-02-03 02:13:29 +0200

    Movie Crawler fixed with messaging.

--- MovieCatalogCrawler/src/main/java/moviecrawler/schedulers/Scheduler.java
+++ MovieCatalogCrawler/src/main/java/moviecrawler/schedulers/Scheduler.java
@@ -1,59 +1,39 @@
 package moviecrawler.schedulers;
 
+import moviecrawler.crawlers.BasicWebCrawler;
 import moviecrawler.crawlers.Crawler;
 import moviecrawler.crawlers.URLEnums;
-import moviecrawler.crawlers.BasicWebCrawler;
-import moviecrawler.entities.Movie;
-import moviecrawler.repositories.MovieRepository;
+import moviecrawler.services.MessagingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import javax.transaction.Transactional;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
+
 
 @Component
 @PropertySource("classpath:scheduler.properties")
 public class Scheduler {
 
     @Autowired
-    private MovieRepository movieRepository;
+    private MessagingService messagingService;
 
     @Scheduled(fixedDelayString = "${schedule.timer}")
-    @Transactional
     public void populateMovies() throws IOException {
         Crawler crawler = new BasicWebCrawler();
 
+        List<String> crawledMovies = new ArrayList<>();
+
         for (URLEnums url : URLEnums.values()) {
-            Set<Movie> moviesInDatabase = new HashSet<>(movieRepository.findAll());
-            final List<String> crawledMovies = crawler.getMovies(url);
-
-            List<String> moviesInDatabaseTitles = moviesInDatabase.stream()
-                    .map(Movie::getTitle)
-                    .collect(Collectors.toList());
-
-            filterAndSaveNewMovies(crawledMovies, moviesInDatabaseTitles);
-
-            deleteExpiredMovies(crawledMovies, moviesInDatabaseTitles);
+            crawledMovies.addAll(crawler.getMovies(url));
         }
+
+        messagingService.sendMessage(crawledMovies);
+
     }
 
-    private void filterAndSaveNewMovies(List<String> crawledMovies, List<String> moviesInDatabaseTitles) {
-        crawledMovies.stream()
-                .filter(m -> !moviesInDatabaseTitles.contains(m))
-                .map(Movie::new)
-                .forEach(m -> movieRepository.saveAndFlush(m));
-    }
 
-    private void deleteExpiredMovies(List<String> crawledMovies, List<String> moviesInDatabaseTitles) {
-        moviesInDatabaseTitles.stream()
-                .filter(m -> !crawledMovies.contains(m))
-                .forEach(m -> movieRepository.deleteMovieByTitle(m));
-    }
 
 }
